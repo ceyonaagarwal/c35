2872746841a7c6b5fe787df003e4cab7094bbecb
ceyonaagarwal <70641231+ceyonaagarwal@users.noreply.github.com>
2020-11-23 18:40:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var canvas, database;
+var gameState = 0;
+var playerCount;
+var form,game,player;
+
+function setup(){
+  database = firebase.database();
+  canvas = createCanvas(400,400);
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  background("white");
+  
+}
